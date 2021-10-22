5b93fbe4efb120f86cb9c7885d2537083e7cde55
Gerrit Hauschildt <gerrit.hauschildt@echolon.de>
2021-10-22 16:49:28 +0200

    UI Beispiel

--- BSELogETL/Form1.cs
+++ BSELogETL/Form1.cs
@@ -16,5 +16,30 @@ namespace BSELogETL
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
