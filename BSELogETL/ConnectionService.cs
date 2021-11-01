25ec4acac4f82c916d4513f8feb7731da35e54cb
Gerrit Hauschildt <gerrit.hauschildt@echolon.de>
2021-11-01 11:00:02 +0100

    Connection Service

---
+++ BSELogETL/ConnectionService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.Sqlite;
+
+namespace BSELogETL
+{
+    public class ConnectionService
+    {
+        public ConnectionService()
+        {
+        }
+
+        private SqliteConnection CreateConnection()
+        {
+            var connectionString = new SqliteConnectionStringBuilder();
+            connectionString.DataSource = "./logBase.sqlite";
+            var connection = new SqliteConnection(connectionString.ConnectionString);
+            
+            return connection;
+        }
+
+        public IEnumerable<string> QueryCommand<T>(string command)
+        {
+            IEnumerable<string> mock = new []{""};
+            var con = CreateConnection();
+            con.Open();
+            var com = con.CreateCommand();
+            com.CommandText = "Hier SQL Query einfügen";
+            var result = com.ExecuteScalar();
+            // return result;
+            
+            return mock;
+        }
+    }
+}
