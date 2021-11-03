175e752173c6f25ba5dd7508374a6163deff662f
Gerrit Hauschildt <gerrit.hauschildt@echolon.de>
2021-11-03 10:38:10 +0100

    Readme and LogEntry Class

---
+++ BSELogETL/LogEntry.cs
@@ -0,0 +1,12 @@
+﻿namespace BSELogETL
+{
+    public class LogEntry
+    {
+        public string IpAddress { get; set; }
+        public string HttpMethod { get; set; }
+        public string HttpLocation { get; set; }
+        public string HttpCode { get; set; }
+        public string RequestedAt { get; set; }
+        public string PackageSize { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # BSELogETL
 ToDo:
-Database anbinden
+-Database connection
+-Parse Logfile
+-Filter 1 component
+-Filter 2 component
+-Filter 3 component
+-Filter 4 component
